ea24ad601bc6b4d1867e42c8cef8b1ff0d28cd18
le gia huy <giahuyle1030@gmail.com>
2024-10-30 16:26:49 +0700

    connect sqldb with sqlalchemy

--- saleapp/app/__init__.py
+++ saleapp/app/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import quote
+from flask import Flask
 
 app = Flask(__name__)
 
--- saleapp/app/models.py
+++ saleapp/app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
-from flask import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app import app, db
 
 
@@ -12,9 +13,45 @@ class Category(db.Model):
 class Product(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
+    description = Column(String(255), nullable=True)
+    image = Column(String(100), nullable=True)
     price = Column(Float, default=0)
     category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
 
+
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        # db.create_all()
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        # c3 = Category(name='Desktop')
+        #
+        # db.session.add(c3)
+        # db.session.commit()
+        data = [{
+            "id": 1,
+            "name": "iPhone 7 Plus",
+            "description": "Apple, 32GB, RAM: 3GB, iOS13",
+            "price": 17000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+            "category_id": 1
+        }, {
+            "id": 2,
+            "name": "iPad Pro 2020",
+            "description": "Apple, 128GB, RAM: 6GB",
+            "price": 37000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+            "category_id": 2
+        }, {
+            "id": 3,
+            "name": "Galaxy Note 10 Plus",
+            "description": "Samsung, 64GB, RAML: 6GB",
+            "price": 24000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+            "category_id": 1
+        }]
+        # them vao database
+        # for i in data:
+        #     db.session.add(name=i['name'], description=i[''])
+        #
+        # db.session.commit()
